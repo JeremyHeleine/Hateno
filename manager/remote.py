4d260c014c818f56087117e239c54550b31024b4
Jérémy Helein <jeremy.heleine@gmail.com>
2020-01-13 17:28:54 +0100

    sendDir() function

--- manager/remote.py
+++ manager/remote.py
@@ -55,7 +55,22 @@ class RemoteFolder():
 		except AttributeError:
 			pass
 
-	def sendFile(self, filename, remote_path = None, *, permissions = -1, delete = False):
+	def copyChmod(self, filename, remote_path):
+		'''
+		Change the chmod of a remote file to reflect a local one.
+
+		Parameters
+		----------
+		filename : str
+			Name of the file to use to copy the chmod.
+
+		remote_path : str
+			Remote path to alter.
+		'''
+
+		self._sftp.chmod(remote_path, os.stat(filename).st_mode & 0o777)
+
+	def sendFile(self, filename, remote_path = None, *, copy_permissions = True, delete = False):
 		'''
 		Send a file.
 
@@ -79,8 +94,39 @@ class RemoteFolder():
 
 		self._sftp.put(filename, remote_path)
 
-		if permissions >= 0:
-			self._sftp.chmod(remote_path, permissions)
+		if copy_permissions:
+			self.copyChmod(filename, remote_path)
 
 		if delete:
 			os.unlink(filename)
+
+	def sendDir(self, directory, remote_path = None, *, copy_permissions = True, delete = False):
+		'''
+		Send a directory.
+
+		Parameters
+		----------
+		directory : str
+			Name of the directory to send.
+
+		remote_path : str
+			Path of the remote directory to create.
+		'''
+
+		if not(remote_path):
+			remote_path = os.path.basename(os.path.normpath(directory))
+
+		try:
+			entries = self._sftp.listdir(remote_path)
+
+		except FileNotFoundError:
+			self._sftp.mkdir(remote_path)
+
+		if copy_permissions:
+			self.copyChmod(directory, remote_path)
+
+		for entry in [(entry, os.path.join(directory, entry)) for entry in os.listdir(directory)]:
+			(self.sendDir if os.path.isdir(entry[1]) else self.sendFile)(entry[1], os.path.join(remote_path, entry[0]), copy_permissions = copy_permissions, delete = delete)
+
+		if delete:
+			os.rmdir(directory)
