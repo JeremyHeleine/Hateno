04c6ed2a8d8e2692220fff1fe388f6f330fe96eb
Jérémy Helein <jeremy.heleine@gmail.com>
2020-01-26 12:42:30 +0100

    Prevent the use of unknown settings

--- manager/folder.py
+++ manager/folder.py
@@ -31,17 +31,65 @@ class Folder():
 
 		self._settings = jsonfiles.read(self._settings_file)
 
-	def generateSettings(self, settings, as_strings = False):
+	# def generateSettings(self, settings, as_strings = False):
+	# 	'''
+	# 	Generate the full set of settings for a simulation.
+	#
+	# 	Parameters
+	# 	----------
+	# 	settings : dict
+	# 		Values of some settings for the simulation.
+	#
+	# 	as_strings : boolean
+	# 		`True` to get a string representation of the settings, `False` to get them as a dictionary.
+	#
+	# 	Returns
+	# 	-------
+	# 	full_settings : dict
+	# 		The full set of settings, with default values if needed.
+	# 	'''
+	#
+	# 	full_settings = []
+	# 	for settings_set in self._settings['settings']:
+	# 		settings_pairs = {s['name']: s['default'] for s in settings_set['settings']}
+	# 		values_sets = [s['settings'] for s in settings if s['set'] == settings_set['set']]
+	#
+	# 		sets_to_add = []
+	# 		for values_set in values_sets:
+	# 			pairs = copy.deepcopy(settings_pairs)
+	# 			pairs.update(values_set)
+	# 			sets_to_add.append(pairs)
+	#
+	# 		if not(sets_to_add) and settings_set['required']:
+	# 			sets_to_add.append(settings_pairs)
+	#
+	# 		if as_strings:
+	# 			patterns = {
+	# 				s['name']: s['pattern'] if 'pattern' in s else self._settings['setting_pattern']
+	# 				for s in settings_set['settings']
+	# 			}
+	#
+	# 			full_settings += [
+	# 				[
+	# 					patterns[setting_name].format(name = setting_name, value = setting_value)
+	# 					for setting_name, setting_value in set_to_add.items()
+	# 				]
+	# 				for set_to_add in sets_to_add
+	# 			]
+	#
+	# 		else:
+	# 			full_settings += sets_to_add
+	#
+	# 	return full_settings
+
+	def generateSettings(self, simulation):
 		'''
 		Generate the full set of settings for a simulation.
 
 		Parameters
 		----------
-		settings : dict
-			Values of some settings for the simulation.
-
-		as_strings : boolean
-			`True` to get a string representation of the settings, `False` to get them as a dictionary.
+		simulation : dict
+			Simulation's informations and settings.
 
 		Returns
 		-------
@@ -52,32 +100,24 @@ class Folder():
 		full_settings = []
 		for settings_set in self._settings['settings']:
 			settings_pairs = {s['name']: s['default'] for s in settings_set['settings']}
-			values_sets = [s['settings'] for s in settings if s['set'] == settings_set['set']]
+			values_sets = [s['settings'] for s in simulation['settings'] if s['set'] == settings_set['set']]
 
 			sets_to_add = []
 			for values_set in values_sets:
 				pairs = copy.deepcopy(settings_pairs)
-				pairs.update(values_set)
+
+				for setting_name in pairs:
+					try:
+						pairs[setting_name] = values_set[setting_name]
+
+					except KeyError:
+						pass
+
 				sets_to_add.append(pairs)
 
 			if not(sets_to_add) and settings_set['required']:
 				sets_to_add.append(settings_pairs)
 
-			if as_strings:
-				patterns = {
-					s['name']: s['pattern'] if 'pattern' in s else self._settings['setting_pattern']
-					for s in settings_set['settings']
-				}
-
-				full_settings += [
-					[
-						patterns[setting_name].format(name = setting_name, value = setting_value)
-						for setting_name, setting_value in set_to_add.items()
-					]
-					for set_to_add in sets_to_add
-				]
-
-			else:
-				full_settings += sets_to_add
+			full_settings += sets_to_add
 
 		return full_settings
