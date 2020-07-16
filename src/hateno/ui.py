f3affd78d71841f32a61d1b5cdd50338e675720e
Jérémy Heleine <jeremy.heleine@gmail.com>
2020-07-16 15:37:33 +0200

    Progress bar class

--- src/hateno/ui.py
+++ src/hateno/ui.py
@@ -479,3 +479,114 @@ class UITextLine(UIDisplayedItem):
 		self.clear()
 		self._text = new_text
 		self.render()
+
+class UIProgressBar(UIDisplayedItem):
+	'''
+	Represent a progress bar displayed in the UI.
+
+	Parameters
+	----------
+	ui : UI
+		The UI object this text line belongs to.
+
+	total : int
+		The final number to reach.
+
+	bar_length : int
+		Length of the progress bar.
+
+	empty_char : str
+		Character to use for the empty part of the bar.
+
+	full_char : str
+		Character to use to fill the bar.
+	'''
+
+	def __init__(self, ui, total, *, bar_length = 40, empty_char = '░', full_char = '█'):
+		super().__init__(ui)
+
+		self._total = total
+		self._counter = 0
+
+		self._bar_length = bar_length
+		self._empty_char = empty_char
+		self._full_char = full_char
+
+		self._pattern = ' '.join([
+			f'{{counter:>{len(str(self._total))}d}}/{self._total}',
+			f'{{bar:{self._empty_char}<{self._bar_length}}}',
+			f'{{percentage:>6.1%}}'
+		])
+
+	@property
+	def height(self):
+		'''
+		The number of lines used by the progress bar.
+
+		Returns
+		-------
+		height : int
+			The number of lines used by the progress bar.
+		'''
+
+		return 1
+
+	@property
+	def width(self):
+		'''
+		The width of the progress bar.
+
+		Returns
+		-------
+		width : int
+			The complete width (counter + bar + percentage lengths).
+		'''
+
+		return len(self._pattern.format(counter = 0, bar = '', percentage = 0))
+
+	@property
+	def counter(self):
+		'''
+		The current value of the counter.
+
+		Returns
+		-------
+		counter : int
+			Counter value.
+		'''
+
+		return self._counter
+
+	def render(self):
+		'''
+		Print the progress bar.
+		'''
+
+		percentage = self._counter / self._total
+		n_full_chars = round(percentage * self._bar_length)
+
+		self.ui.moveCursorTo(self.position)
+		print(self._pattern.format(counter = self._counter, bar = self._full_char * n_full_chars, percentage = percentage), end = '\r')
+		self.ui.moveToLastLine()
+
+	@counter.setter
+	def counter(self, n):
+		'''
+		Set the value of the counter.
+		'''
+
+		self.clear()
+		self._counter = n
+		self.render()
+
+	def update(self, delta = 1):
+		'''
+		Set the value of the counter by adding an increment.
+
+		Parameters
+		----------
+		delta : int
+			Increment to add.
+		'''
+
+		self.counter += delta
